21068100752bf84bccf7a89584122bb3c87e9256
Arnold Restrepo <restrepo_arnold@outlook.com>
2023-03-13 22:08:57 -0500

    Experience Info

---
+++ src/lib/experience.ts
@@ -0,0 +1,70 @@
+import { Experience } from '@root/models'
+
+export const experienceData: Experience[] = [
+  {
+    date: "Abril 2021 - Diciembre 2022",
+    title: "UX/UI Designer",
+    subtitle: "150 POR CIENTO, Bogotá",
+    open: true,
+    responsibilities: [
+      {
+        item: "Definición y aplicación del estilo gráfico que se implementó en proyectos como el site de BBC Cervecería, aplicación Runnder, landing page Corona, páginas internas de Primax, entre otros. Teniendo como base el branding de cada marca."
+      },
+      {
+        item: "Creación de mockups responsive empleando la herramienta Figma con el fin de dar respuesta a una solicitud del cliente, ya sea re-diseño o planteamiento de la idea desde cero."
+      },
+      {
+        item: "Implementación de prototipos por medio de Figma con el fin de dar una experiencia cercana al producto final."
+      }
+    ],
+    achievements: [
+      {
+        item: "Creé e implementé el Sistema de Diseño interno de la empresa con su respectiva documentación para facilitar la comprensión del mismo. Logré reducir los tiempos de entrega por medio de la estandarización de componentes."
+      },
+      {
+        item: "Implementé el uso de portadas de proyectos empleando Figma, de esta manera se mejoró la organización de los mismos facilitando su búsqueda y fácil identificación del estado actual teniendo en cuenta el flujo interno del proyecto. Realice su respectiva documentación."
+      }
+    ]
+  },
+  {
+    date: "Abril 2017 - Enero 2021",
+    title: "Associate Front-End Engineer",
+    subtitle: "Prodigious, Bogotá",
+    open: false,
+    responsibilities: [
+      {
+        item: "Creación de correos empleando HTML 4 y CSS. Las pruebas en diferentes clientes de correos se llevan a cabo por medio de Litmus, Email on acid, Putsmail y campaign monitor. Con el fin de conocer y emplear un flujo de trabajo se emplea la herramienta JIRA"
+      },
+      {
+        item: "Desarrollo de banners animados con el uso de HTML5, CSS, JavaScript y la librería GreenSock. Uso de la herramienta Photoshop para obtener imágenes y textos."
+      },
+      {
+        item: "Participación en el equipo de Front-end que llevó a cabo la página web del evento Digital Up Level Summit. Realizando reuniones con la metodología Scrum y aplicando herramientas como HTML5, CSS3, React, Jira y Git."
+      }
+    ],
+    achievements: [
+      {
+        item: "Dentro de la participación en el site para el evento Digital Up Level Summit, realice la animación de las cards que contienen información relacionada con los Speakers, con esto mejoré la interacción con el componente, dicha animación se sigue implementando."
+      }
+    ]
+  },
+  {
+    date: "Octubre 2016 - Febrero 2017",
+    title: "Practicante SENA",
+    subtitle: "Prodigious, Bogotá",
+    open: false,
+    responsibilities: [
+      {
+        item: "Diseño de Wireframes. Con un equipo de trabajo desarrollamos el home de una tienda online responsive, por medio de HTML5, CSS3, SASS, Git y Metodología Scrum.",
+      },
+      {
+        item: "Participación en el proyecto Snow Box, desarrollando el User Flow por medio de Adobe XD y la creación de personajes empleando Photoshop."
+      }
+    ],
+    achievements: [
+      {
+        item: "Recibí un reconocimiento Kudos junto al equipo de trabajo por el proyecto Snow Box."
+      }
+    ]
+  }
+]
--- src/models.ts
+++ src/models.ts
@@ -5,4 +5,18 @@ export interface WorkProps {
     heroImage: string;
     date?: string; 
   }
-} 
+}
+
+export interface ExperienceItem {
+  item: string
+}
+
+export interface Experience {
+  date: string;
+  title: string;
+  subtitle: string;
+  open: boolean;
+  responsibilities: ExperienceItem[];
+  achievements: ExperienceItem[];
+}
+
